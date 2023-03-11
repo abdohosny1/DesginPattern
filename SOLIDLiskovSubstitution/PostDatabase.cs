f29c111ed6939b14457d077a8ee1b006d744e629
abdohosny1 <make.y90@yahoo.com>
2023-03-11 17:14:28 +0200

    add Liskov Substitution

---
+++ SOLIDLiskovSubstitution/PostDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDLiskovSubstitution
+{
+    public class PostDatabase
+    {
+        public string Add(string post)
+        {
+            return "Original Post: " + post;
+        }
+        public string AddTagPost(string post)
+        {
+            return "Tag post: " + post;
+        }
+        public string AddMentionPost(string post)
+        {
+            return "Mention post: " + post;
+        }
+    }
+}
---
+++ SOLIDLiskovSubstitution/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOLIDLiskovSubstitution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PostDatabase db = new PostDatabase();
+            List<string> newPosts = new List<string>();
+            newPosts.Add("original post");
+            newPosts.Add("#tag post");
+            newPosts.Add("@mention post");
+            Post postObj;
+            foreach (var post in newPosts)
+            {
+                if (post.StartsWith("#"))
+                {
+                    postObj = new TagPost();
+
+                }
+                else if (post.StartsWith("@"))
+                {
+                    postObj = new MentionPost();
+                }
+                else
+                {
+                    postObj = new Post();
+                }
+                    string result = postObj.CreatePost(db, post);
+                    Console.WriteLine(result);
+                }
+                Console.ReadKey();
+            }
+        }
+
+        public class Post {
+            public virtual string CreatePost(PostDatabase db, string post)
+            {
+                return db.Add(post);
+            }
+        }
+        public class TagPost : Post
+        {
+            public override string CreatePost(PostDatabase db, string post)
+            {
+                return db.AddTagPost(post);
+            }
+        }
+
+        public class MentionPost : Post
+        {
+            public override string CreatePost(PostDatabase db, string post)
+            {
+                return db.AddMentionPost(post);
+            }
+        }
+
+    
+    }
+    
