419bae2cf6c7d9f51ee13b29cd221e92e1b420e4
abdohosny1 <make.y90@yahoo.com>
2023-03-12 13:04:17 +0200

    add  Interface Segregation

---
+++ SOLID Interface Segregation/BadSoluation.cs
@@ -0,0 +1,91 @@
+﻿//using System;
+
+//namespace SOLID_Interface_Segregation
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+
+//        public class CashOrder : IOrder
+//        {
+//            public void InsallmentOrder()
+//            {
+//                throw new NotImplementedException();
+//            }
+
+//            public void ProcessCashInvoice()
+//            {
+//                throw new NotImplementedException();
+//            }
+
+//            public void ProcessCreditInvoice()
+//            {
+//                throw new NotImplementedException();
+//            }
+
+//            public void ProcessOnlinePaymentInvoice()
+//            {
+//                throw new NotImplementedException();
+//            }
+//        }
+
+//        public class OnlineOrder : IOrder
+//        {
+//            public void InsallmentOrder()
+//            {
+//                throw new NotImplementedException();
+//            }
+
+//            public void ProcessCashInvoice()
+//            {
+//                throw new NotImplementedException();
+//            }
+
+//            public void ProcessCreditInvoice()
+//            {
+//                throw new NotImplementedException();
+//            }
+
+//            public void ProcessOnlinePaymentInvoice()
+//            {
+//                throw new NotImplementedException();
+//            }
+//        }
+
+//        public class InsatllmentOrder : IOrder
+//        {
+//            public void InsallmentOrder()
+//            {
+//                throw new NotImplementedException();
+//            }
+
+//            public void ProcessCashInvoice()
+//            {
+//                throw new NotImplementedException();
+//            }
+
+//            public void ProcessCreditInvoice()
+//            {
+//                throw new NotImplementedException();
+//            }
+
+//            public void ProcessOnlinePaymentInvoice()
+//            {
+//                throw new NotImplementedException();
+//            }
+//        }
+
+//        public interface IOrder
+//        {
+
+//            void ProcessCashInvoice();
+//            void ProcessCreditInvoice();
+//            void ProcessOnlinePaymentInvoice();
+
+//            void InsallmentOrder();
+//        }
+//    }
+//}
---
+++ SOLID Interface Segregation/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SOLID_Interface_Segregation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class CashOrder : ICashOrder
+        {
+            public void ProcessCashInvoice()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void shareMothod()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class OnlineOrder : IOnlineOrder
+        {
+            public void ProcessOnlinePaymentInvoice()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void shareMothod()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class WorkONAllPay : IWorkONAllPay
+        {
+            public void ProcessCashInvoice()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void ProcessCreditInvoice()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void ProcessOnlinePaymentInvoice()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void shareMothod()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public interface IOrder
+        {
+            void shareMothod();
+        }
+        public interface ICashOrder : IOrder { void ProcessCashInvoice(); }
+        public interface IOnlineOrder : IOrder { void ProcessOnlinePaymentInvoice(); }
+        public interface ICreditOrderIOrder : IOrder { void ProcessCreditInvoice(); }
+
+
+        public interface IWorkONAllPay:ICashOrder,IOnlineOrder,ICreditOrderIOrder { }
+
+    }
+}
