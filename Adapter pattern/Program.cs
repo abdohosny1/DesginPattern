08a24e2f1dcb9be6b1686e0812a8e535f4104e4a
abdohosny1 <make.y90@yahoo.com>
2023-04-01 15:23:49 +0200

    add Adapter pattern

---
+++ Adapter pattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Adapter_pattern
+{
+    internal class Program
+    {
+        static void WriteColorLine(string text, ConsoleColor consoleColor = ConsoleColor.Green)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(text);
+        }
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee { Name = "Test", BasicSalary = 1000 };
+            MachineOperator machineOperator= new MachineOperator { Name= "Machine",BasicSalary=2000,ShiftCode=12 };
+            SalaryAdapter salaryCalculator=new SalaryAdapter();
+            var res=salaryCalculator.calaSalary(employee);
+            var resMachine=salaryCalculator.calaSalary(machineOperator);
+            WriteColorLine(res.ToString(), ConsoleColor.Green);
+            WriteColorLine(resMachine.ToString(), ConsoleColor.Red);
+        }
+    }
+}
---
+++ Adapter pattern/SalaryAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_pattern
+{
+    public class SalaryAdapter :SalaryCalculator
+    {
+        private Employee employee;
+
+        public double calaSalary(MachineOperator machine)
+        {
+            employee=new Employee();
+            employee.BasicSalary = machine.BasicSalary;
+            return base.calaSalary(employee);
+
+        }
+
+    }
+}
